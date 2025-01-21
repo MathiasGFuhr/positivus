28c87e9e0bb20d046d8bc0c3f3da0d1e61f9171d
MathiasGFuhr <mathiasgfuhr@gmail.com>
2025-01-21 12:04:36 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+const menuToggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('header nav');
+
+menuToggle.addEventListener('click', () => {
+    nav.classList.toggle('open');
+});
+
